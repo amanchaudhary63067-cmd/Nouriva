abc9af176fbcb5bfabaa42ed217d87bc91219456
amanchaudhary63067-cmd <amanchaudhary63067@gmail.com>
2026-09-25 15:05:57 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,18 +8,31 @@ import { getFirestore } from
 "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
 
+// Firebase configuration
 const firebaseConfig = {
-    // YAHAN Firebase Console se copied values paste karo
+    apiKey: "AIzaSyBA6eBBT30P1q79cRZfFRFVGCEaDcM5Xfk",
+    authDomain: "nouriva-f4d46.firebaseapp.com",
+    projectId: "nouriva-f4d46",
+    storageBucket: "nouriva-f4d46.firebasestorage.app",
+    messagingSenderId: "359922533027",
+    appId: "1:359922533027:web:1e735f66237e36c169f516",
+    measurementId: "G-Z12HG4GSPX"
 };
 
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
+// Firebase Authentication
 const auth = getAuth(app);
 
+
+// Firestore Database
 const db = getFirestore(app);
 
 
+// Export
 export {
     app,
     auth,
