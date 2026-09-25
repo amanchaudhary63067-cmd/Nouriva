3f1db82a6e122fab7382fffc923d3010c28a396d
amanchaudhary63067-cmd <amanchaudhary63067@gmail.com>
2026-09-25 14:57:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,17 +9,7 @@ import { getFirestore } from
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBA6eBBT30P1q79cRZfFRFVGC...",
-
-    authDomain: "nouriva-f4d46.firebaseapp.com",
-
-    projectId: "nouriva-f4d46",
-
-    storageBucket: "nouriva-f4d46.firebasestorage.app",
-
-    messagingSenderId: "359922533027",
-
-    appId: "1:359922533027:web:1e735f66237e36c169f516"
+    // YAHAN Firebase Console se copied values paste karo
 };
 
 
