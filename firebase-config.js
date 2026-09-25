9cd1ee72dd957dddd98706a338c410a517f9b066
amanchaudhary63067-cmd <amanchaudhary63067@gmail.com>
2026-09-25 14:28:05 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,37 @@
-rules_version = '2';
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
-service cloud.firestore {
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
-    match /databases/{database}/documents {
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
-        match /users/{userId} {
 
-            allow read, write:
-                if request.auth != null
-                && request.auth.uid == userId;
+const firebaseConfig = {
+    apiKey: "AIzaSyBA6eBBT30P1q79cRZfFRFVGC...",
 
-        }
+    authDomain: "nouriva-f4d46.firebaseapp.com",
 
-    }
+    projectId: "nouriva-f4d46",
 
-}
+    storageBucket: "nouriva-f4d46.firebasestorage.app",
+
+    messagingSenderId: "359922533027",
+
+    appId: "1:359922533027:web:1e735f66237e36c169f516"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+export {
+    app,
+    auth,
+    db
+};
